0fc80d92f2499b4653e92fc5a35a06fc568cbe8e
Spencer White <swhit210@ford.com>
2019-11-25 13:40:41 -0500

    script to read multiple documents

---
+++ couchdb/bash/read_document_DB.sh
@@ -0,0 +1,16 @@
+# Get Document Information
+# Get Document Information
+curl -X GET 'http://localhost:5984/heroes/JOXOS'
+curl -X GET 'http://localhost:5984/heroes/VI'
+curl -X GET 'http://localhost:5984/heroes/ROKH_AEGIS'
+curl -X GET 'http://localhost:5984/heroes/TRANQUILITY'
+curl -X GET 'http://localhost:5984/heroes/BEATRIX_EMORY'
+curl -X GET 'http://localhost:5984/heroes/INFERNO_FLARE'
+
+curl -I HEAD 'http://localhost:5984/heroes/JOXOS'
+curl -I HEAD 'http://localhost:5984/heroes/VI'
+curl -I HEAD 'http://localhost:5984/heroes/ROKH_AEGIS'
+curl -I HEAD 'http://localhost:5984/heroes/TRANQUILITY'
+curl -I HEAD 'http://localhost:5984/heroes/BEATRIX_EMORY'
+curl -I HEAD 'http://localhost:5984/heroes/INFERNO_FLARE'
+
