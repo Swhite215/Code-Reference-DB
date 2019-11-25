0e3308f7fc9c3b3ced4a77aeb9e04dd0e9961393
Spencer White <swhit210@ford.com>
2019-11-25 13:57:54 -0500

    two queries to get all documents

--- couchdb/bash/read_DB.sh
+++ couchdb/bash/read_DB.sh
@@ -11,3 +11,13 @@ curl -X GET 'http://localhost:5984/heroes'
 
 echo "curl -I HEAD 'http://localhost:5984/heroes'" 
 curl -I HEAD 'http://localhost:5984/heroes'
+
+# Get All Documents in Database
+echo "curl -X GET 'http://localhost:5984/heroes/_all_docs'" 
+curl -X GET 'http://localhost:5984/heroes/_all_docs'
+
+# Find All Documents that Satisfy Parameters
+echo "curl -H 'Content-Type:application/json' -X POST 'http://localhost:5984/heroes/_find' -d {JSON}"
+curl -H 'Content-Type:application/json' -X POST 'http://localhost:5984/heroes/_find' -d '{"selector": {"health": { "$gt": 100 }}, "fields": ["name", "health"], "limit": 20}'
+
+# Find All Document Changes
