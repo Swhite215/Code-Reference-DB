c34efe8c6e29945a7dc5fd9ad126c08f43eca31e
Spencer White <swhit210@ford.com>
2019-11-25 14:04:04 -0500

    queries to get revision information

--- couchdb/bash/read_document_DB.sh
+++ couchdb/bash/read_document_DB.sh
@@ -14,3 +14,18 @@ curl -I HEAD 'http://localhost:5984/heroes/TRANQUILITY'
 curl -I HEAD 'http://localhost:5984/heroes/BEATRIX_EMORY'
 curl -I HEAD 'http://localhost:5984/heroes/INFERNO_FLARE'
 
+# Get Document List of Revisions
+curl -X GET 'http://localhost:5984/heroes/JOXOS?revs=true'
+curl -X GET 'http://localhost:5984/heroes/VI?revs=true'
+curl -X GET 'http://localhost:5984/heroes/ROKH_AEGIS?revs=true'
+curl -X GET 'http://localhost:5984/heroes/TRANQUILITY?revs=true'
+curl -X GET 'http://localhost:5984/heroes/BEATRIX_EMORY?revs=true'
+curl -X GET 'http://localhost:5984/heroes/INFERNO_FLARE?revs=true'
+
+# Get Document Revision History
+curl -X GET 'http://localhost:5984/heroes/JOXOS?revs_info=true'
+curl -X GET 'http://localhost:5984/heroes/VI?revs_info=true'
+curl -X GET 'http://localhost:5984/heroes/ROKH_AEGIS?revs_info=true'
+curl -X GET 'http://localhost:5984/heroes/TRANQUILITY?revs_info=true'
+curl -X GET 'http://localhost:5984/heroes/BEATRIX_EMORY?revs_info=true'
+curl -X GET 'http://localhost:5984/heroes/INFERNO_FLARE?revs_info=true'
