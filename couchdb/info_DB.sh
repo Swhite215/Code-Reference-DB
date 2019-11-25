cb624923c8dde055b1d96b335675efdb21770925
Spencer White <swhit210@ford.com>
2019-11-25 10:53:53 -0500

    added GET to specific DB

--- couchdb/info_DB.sh
+++ couchdb/info_DB.sh
@@ -1,6 +1,11 @@
 # Get Database Information - curl http://domain:port or curl -X METHOD http://domain:port
+echo "curl 'http://localhost:5984'"
+
 curl 'http://localhost:5984'
 
+echo "curl -X GET 'http://localhost:5984'" 
 curl -X GET 'http://localhost:5984'
 
 
+echo "curl -X GET 'http://localhost:5984/heroes'" 
+curl -X GET 'http://localhost:5984/heroes'
