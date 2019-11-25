144309da32450e18e3b6e8bf186a236c330b82aa
Spencer White <swhit210@ford.com>
2019-11-25 10:42:47 -0500

    changed to --mount

--- couchdb/start_DB.sh
+++ couchdb/start_DB.sh
@@ -1,4 +1,4 @@
-# Start a CouchDB container with exposed port
-docker run -p 5984:5984 -d --name my-couchdb couchdb:latest
+# Start a CouchDB container with exposed port and bind mount
+docker run -p 5984:5984 --mount type=bind,source=/tmp/couchdb/data,target=/opt/couchdb/data -d --name my-couchdb couchdb:latest
 
 # Fauxton UI - localhost:5984/_utils/
