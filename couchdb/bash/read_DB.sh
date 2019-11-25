29cb9a8ef40a44ad04bfc924933c2e88c9f77a2d
Spencer White <swhit210@ford.com>
2019-11-25 13:40:29 -0500

    script to read a DB

---
+++ couchdb/bash/read_DB.sh
@@ -0,0 +1,13 @@
+# Get CouchDB Information
+echo "curl 'http://localhost:5984'"
+curl 'http://localhost:5984'
+
+echo "curl -X GET 'http://localhost:5984'" 
+curl -X GET 'http://localhost:5984'
+
+# Get Database Information
+echo "curl -X GET 'http://localhost:5984/heroes'" 
+curl -X GET 'http://localhost:5984/heroes'
+
+echo "curl -I HEAD 'http://localhost:5984/heroes'" 
+curl -I HEAD 'http://localhost:5984/heroes'
