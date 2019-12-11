321d7674732c37785f76b911a7bb14504ab73658
Spencer White <swhit210@ford.com>
2019-12-11 14:58:57 -0500

    js script to read a mongodb database

---
+++ mongodb/bash/read_DB.js
@@ -0,0 +1,7 @@
+// Connect to Database
+const conn = new Mongo();
+const db = conn.getDB('Characters');
+
+// List Databases on Server
+let databases = db.adminCommand('listDatabases');
+printjson(databases)
