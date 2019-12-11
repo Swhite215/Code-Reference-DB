a94d47e155635d224ed1b900f813617bf89e53f8
Spencer White <swhit210@ford.com>
2019-12-11 14:40:27 -0500

    script and notes to start mongodb server and gui

--- mongodb/bash/start_DB.sh
+++ mongodb/bash/start_DB.sh
@@ -2,3 +2,4 @@
 docker run -p 27017:27017 --mount type=bind,source=/tmp/mongodb/data,target=/data --name my-mongodb -d mongo 
 
 # MongoDB UI - Compass: https://www.mongodb.com/download-center/compass
+# MongoDB URL - mongodb://localhost:27017
