4aeab6cd73933dcb7e72039236719f448b523214
Spencer White <swhit210@ford.com>
2019-12-11 14:41:25 -0500

    js script to create mongodb collection

--- mongodb/bash/create_collection_DB.js
+++ mongodb/bash/create_collection_DB.js
@@ -1,5 +1,7 @@
-conn = new Mongo();
-db = conn.getDB('Characters');
+// Connect to Database
+const conn = new Mongo();
+const db = conn.getDB('Characters');
 
+// Create Collections in Database
 db.createCollection('Heroes')
 db.createCollection('Villians')
