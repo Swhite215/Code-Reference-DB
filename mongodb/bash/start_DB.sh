5a1d3d9e0e5ecde64fefea5254c361bf247b4f9a
Spencer White <swhit210@ford.com>
2019-12-11 13:31:35 -0500

    added link to mongodb gui compass

--- mongodb/bash/start_DB.sh
+++ mongodb/bash/start_DB.sh
@@ -1,2 +1,4 @@
 # Start a MongoDB container with exposed port and bind mount
 docker run -p 27017:27017 --mount type=bind,source=/tmp/mongodb/data,target=/data --name my-mongodb -d mongo 
+
+# MongoDB UI - Compass: https://www.mongodb.com/download-center/compass
