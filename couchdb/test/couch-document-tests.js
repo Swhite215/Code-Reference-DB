81481429089886bb741c120b13b4d820562f2a0c
Spencer White <swhit210@ford.com>
2020-01-06 13:39:13 -0500

    tests for querying documents using search criteria

--- couchdb/test/couch-document-tests.js
+++ couchdb/test/couch-document-tests.js
@@ -44,7 +44,6 @@ describe("CouchDB Document Endpoints", function() {
             getDocumentStub.withArgs(db, documentID).resolves(expectedRes)
 
             couchDBService.getDocument(db, documentID).then((value) => {
-                console.log(value)
                 expect(value).to.have.any.keys("_id", "_rev");
                 done();
             }).catch(err => console.log);
@@ -62,6 +61,93 @@ describe("CouchDB Document Endpoints", function() {
                 .end((err, res) => {
                     res.should.have.status(200)
                     expect(res.body).to.have.any.keys("_id", "_rev");
+                    done();
+                })
+        });
+    });
+
+    describe("GET /db/:db/query/document - Get documents that meet search criteria", function() {
+
+        let db = "heroes";
+
+        let testQuery = {
+            selector: {
+                name: {"$eq": "Joxos"},
+                stamina: {"$gt": 125}
+            },
+            fields: ["name", "health", "stamina", "atk", "items"],
+            limit: 100
+        }
+
+        let emptyQuery = {
+            selector: {
+                name: {"$eq": "Titan"}
+            },
+            fields: ["name", "health", "stamina", "atk", "items"],
+            limit: 1
+        }
+
+        let queryDocumentsStub;
+
+        let expectedResult = {
+            docs: [{"name": "Joxos"}],
+            bookmark: "TEST BOOKMARK"
+        }
+
+        let expectedEmptyResult = {
+            docs: [],
+            bookmark: "TEST BOOKMARK" 
+        }
+
+        beforeEach(function() {
+            queryDocumentsStub = sinon.stub(couchDBService, "queryDocuments");
+        });
+
+        afterEach(function() {
+            queryDocumentsStub.restore();
+        })
+
+        it("should use a function named queryDocuments", function() {
+            assert.isFunction(couchDBService.queryDocuments);
+        });
+
+        it("queryDocuments should return an object with matching documents", function(done) {
+
+            queryDocumentsStub.withArgs(db, testQuery).resolves(expectedResult);
+
+            couchDBService.queryDocuments(db, testQuery).then((value) => {
+                expect(value).to.have.any.keys("docs", "bookmark");
+                expect(value.docs).to.be.a("array");
+                expect(value.docs.length).to.be.above(0);
+                done();
+            });
+        });
+
+        it("queryDocuments should return an object with no documents if none match criteria", function(done) {
+            
+            queryDocumentsStub.withArgs(db, emptyQuery).resolves(expectedEmptyResult);
+
+            couchDBService.queryDocuments(db, emptyQuery).then((value) => {
+                expect(value).to.have.any.keys("docs", "bookmark");
+                expect(value.docs).to.be.a("array");
+                expect(value.docs.length).to.equal(0);
+                done();
+            });
+        });
+
+        it("should be an endpoint that returns documents that meet search criteria", function(done) {
+
+            queryDocumentsStub.withArgs(db, testQuery).resolves(expectedResult);
+            
+            chai
+                .request(app)
+                .get(`/db/${db}/query/document`)
+                .send(testQuery)
+                .end((err, res) => {
+                    res.should.have.status(200)
+                    expect(res.body).to.have.any.keys("docs", "bookmark");
+                    expect(res.body.docs).to.be.a("array")
+                    expect(res.body.docs.length).to.be.above(0);
                     done();
                 })
         });
