30ccae27226b18994e33b7b63061b66dfbb17115
Spencer White <swhit210@ford.com>
2020-01-27 15:45:02 -0500

    stopping mssql server

--- sql/bash/start_Server.sh
+++ sql/bash/start_Server.sh
@@ -1,1 +1,1 @@
-docker run -e 'HOMEBREW_NO_ENV_FILTERING=1' -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password$1234' -p 1433:1433 -d microsoft/mssql-server-linux
+docker run -e 'HOMEBREW_NO_ENV_FILTERING=4' -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password$1234' -p 1433:1433 --name=my-mssql-server -d microsoft/mssql-server-linux
---
+++ sql/bash/stop_Server.sh
@@ -0,0 +1,8 @@
+# Stop my-mssql-server
+docker stop my-mssql-server
+
+# Remove my-mssql-server
+docker rm my-mssql-server
+
+# Force delete my-mssql-server volume
+docker volume prune --force
