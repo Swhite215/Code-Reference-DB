1da83c615418394edbdbbd7040021fb9e4129988
Spencer White <swhit210@ford.com>
2020-01-27 15:06:54 -0500

    query to insert multiple records

---
+++ sql/bash/insert_Records.sql
@@ -0,0 +1,7 @@
+USE characters;
+INSERT INTO heroes (FirstName, Health, Stamina, Atk, CanFight) VALUES ("Joxos", 250, 150, 25, 1);
+INSERT INTO heroes (FirstName, Health, Mana, Atk, Magic, CanFight, CanCast) VALUES ("Vi", 75, 150, 5, 25, 1, 1);
+INSERT INTO heroes (FirstName, Health, Stamina, Atk, CanFight, CanSteal) VALUES ("Rokh", 100, 125, 35, 1, 1);
+INSERT INTO heroes (FirstName, Health, Stamina, Atk, CanFight) VALUES ("Tranquility", 125, 100, 20, 1);
+INSERT INTO heroes (FirstName, Health, Mana, Atk, Magic, CanFight, CanCast, CanHeal) VALUES ("Beatrix", 125, 125, 15, 20, 1, 1, 1);
+INSERT INTO heroes (FirstName, health, Stamina, Atk, CanFight, CanCast) VALUES ("Inferno", 150, 75, 40, 1, 1);
