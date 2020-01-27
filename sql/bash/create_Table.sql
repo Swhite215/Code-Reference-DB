5334671f86628392ae572d3a0c06af17d4e51580
Spencer White <swhit210@ford.com>
2020-01-27 14:30:00 -0500

    create table query

---
+++ sql/bash/create_Table.sql
@@ -0,0 +1,14 @@
+USE characters;
+
+CREATE TABLE heroes (
+    char_id INT NOT NULL PRIMARY KEY,
+    FirstName varchar(255) NOT NULL,
+    Health int,
+    Stamina int,
+    Atk int,
+    Magic int,
+    CanFight bit,
+    CanSteal bit,
+    CanHeal bit,
+    CanCast bit
+);
