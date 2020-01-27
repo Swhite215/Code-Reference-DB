06acf308a54635b067bcfb4105f1f79992178aeb
Spencer White <swhit210@ford.com>
2020-01-27 14:33:16 -0500

    creates villian table as well

--- sql/bash/create_Table.sql
+++ sql/bash/create_Table.sql
@@ -12,3 +12,16 @@ CREATE TABLE heroes (
     CanHeal bit,
     CanCast bit
 );
+
+CREATE TABLE villians (
+    char_id INT NOT NULL PRIMARY KEY,
+    FirstName varchar(255) NOT NULL,
+    Health int,
+    Stamina int,
+    Atk int,
+    Magic int,
+    CanFight bit,
+    CanSteal bit,
+    CanHeal bit,
+    CanCast bit
+);
