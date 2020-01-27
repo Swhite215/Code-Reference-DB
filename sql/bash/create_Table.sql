3e80472bbddb27698deae07f02c948a277bc21c7
Spencer White <swhit210@ford.com>
2020-01-27 15:02:22 -0500

    query to create tables with primary key

--- sql/bash/create_Table.sql
+++ sql/bash/create_Table.sql
@@ -1,29 +1,29 @@
 USE characters;
 
 CREATE TABLE heroes (
-    char_id INT NOT NULL PRIMARY KEY,
+    char_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
     FirstName varchar(255) NOT NULL,
-    Health int,
-    Stamina int,
-    Mana int,
-    Atk int,
-    Magic int,
-    CanFight bit,
-    CanSteal bit,
-    CanHeal bit,
-    CanCast bit
+    Health int NOT NULL,
+    Stamina int NULL,
+    Mana int NULL,
+    Atk int NULL,
+    Magic int NULL,
+    CanFight bit NULL,
+    CanSteal bit NULL,
+    CanHeal bit NULL,
+    CanCast bit NULL
 );
 
 CREATE TABLE villians (
-    char_id INT NOT NULL PRIMARY KEY,
+    char_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
     FirstName varchar(255) NOT NULL,
-    Health int,
-    Stamina int,
-    Mana int,
-    Atk int,
-    Magic int,
-    CanFight bit,
-    CanSteal bit,
-    CanHeal bit,
-    CanCast bit
+    Health int NOT NULL,
+    Stamina int NULL,
+    Mana int NULL,
+    Atk int NULL,
+    Magic int NULL,
+    CanFight bit NULL,
+    CanSteal bit NULL,
+    CanHeal bit NULL,
+    CanCast bit NULL
 );
