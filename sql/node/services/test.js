bcc4b3be4d25678ef6043a249f07846ce9d3d69b
Spencer White <swhit210@ford.com>
2020-01-31 08:33:08 -0500

    testing push

---
+++ sql/node/services/test.js
@@ -0,0 +1,8 @@
+const sqlService = require("../services/sqlService");
+
+sqlService.queryDatabases().then((res) => {
+    console.log(res);
+}).catch((e) => {
+    console.log(e);
+    console.log(e.message);
+});
