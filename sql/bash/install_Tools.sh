6bc66b6885fd1471010ab5b0e3c5e4b5d205e5b2
Spencer White <swhite215@live.com>
2022-01-10 10:51:02 -0500

    added link to issue I experienced

--- sql/bash/install_Tools.sh
+++ sql/bash/install_Tools.sh
@@ -1,3 +1,5 @@
 brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
 brew update
 brew install msodbcsql mssql-tool
+
+# Helpful Link: https://github.com/microsoft/homebrew-mssql-release/issues/59
